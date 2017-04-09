fbcf747cb5a2a786365891a82c7c352cad8ee62b
kartikvadhavana <kartik_vadhavana@yahoo.com>
2017-04-09 19:08:50 +0200

    working copy 1

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 var express = require('express');
+
+
+module.exports = function () {
+    var app = express();
+
+    app.get('/', function (req, res) {
+        res.send('Hello world');
+    });
+
+    return app;
+}
