8eff6d9fe4509ec684767bd63cbed6280e5d3562
kartikvadhavana <kartik_vadhavana@yahoo.com>
2017-04-11 22:12:41 +0200

    integrated with Jenkins

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ module.exports = function () {
     var app = express();
 
     app.get('/', function (req, res) {
-        res.send('Integrated with Docker!!!');
+        res.send('Integrated with Jenkins!!!');
     });
 
     return app;
