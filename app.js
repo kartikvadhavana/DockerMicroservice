f72a1f2f83eb8c62041194c4e05a5ad195269bbe
kartikvadhavana <kartik_vadhavana@yahoo.com>
2017-04-11 23:21:58 +0200

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ module.exports = function () {
     var app = express();
 
     app.get('/', function (req, res) {
-        res.send('Integrated with docker and jenkins');
+        res.send('Integrated with docker and jenkins!');
     });
 
     return app;
